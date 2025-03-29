860bad18cd7f1e83d0d754b2844634251130d612
ppastorf <pdrpfernandes99@gmail.com>
2025-03-29 18:56:10 -0300

    Updates

--- src/dex-arbitrage/main.py
+++ src/dex-arbitrage/main.py
@@ -12,12 +12,16 @@ from itertools import permutations
 from collections import defaultdict
 from fastapi.responses import JSONResponse
 
+
 #############################
-##### ARGS
+##### UTILS
 #############################
 
 def parse_config_file(filepath: str):
     data = {}
+    if filepath == None:
+      return data
+
     try:
       with open(filepath, "r") as file:
         data = yaml.safe_load(file)
@@ -27,15 +31,81 @@ def parse_config_file(filepath: str):
 
     return data
 
+def defaultdict_to_dict(d):
+    if isinstance(d, defaultdict):
+        d = {key: defaultdict_to_dict(value) for key, value in d.items()}
+    return d
+
+def get_now_timestamp():
+  return datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d_%H:%M:%S.%f")
+
+def connect_to_rpc_node(rpc_url: str):
+    rpc_conn = Web3(Web3.HTTPProvider(rpc_url))
+    if not rpc_conn.is_connected():
+        print("Failed to connect to Ethereum node.")
+        return None
+
+    return rpc_conn
+
+def get_abi_from_file(abi_filepath: str):
+    print(f"Getting ABI at {abi_filepath}...")
+    abi = {}
+    try:
+      with open(abi_filepath, "r") as file:
+        abi = json.load(file)
+    except Exception as e:
+      print(f"Error loading pair ABI at {abi_filepath}: {e}")
+      return None
+
+    return abi
+
+#############################
+##### CLASSES
+#############################
+
+class ERC20Token:
+  def __init__(self, name, address: str, decimals: int):
+     self.decimals = decimals
+     self.address = Web3.to_checksum_address(address)
+     self.name = name
+     self.abi = get_abi_from_file("abi/erc20.json")
+
+class UniswapTokenPair:
+  def __init__(self, type, exchange_name, token0, token1, contract_address, router_address=None):
+    self.exchange_name = exchange_name
+    self.type = type
+    self.token0 = token0
+    self.token1 = token1
+    self.name = f"{token0.name}/{token1.name}"
+
+    match self.type:
+      case "uniswap-v2":
+        self.router_contract = rpc_conn.eth.contract(
+          address=Web3.to_checksum_address(router_address),
+          abi=get_abi_from_file("abi/uniswap-v2-router02.json")
+        )
+        self.pair_contract = rpc_conn.eth.contract(
+          address=Web3.to_checksum_address(contract_address),
+          abi=get_abi_from_file("abi/uniswap-v2-pool.json")
+        )
+      case "uniswap-v3":
+        self.router_contract = None
+        self.pair_contract = rpc_conn.eth.contract(
+          address=Web3.to_checksum_address(contract_address),
+          abi=get_abi_from_file("abi/uniswap-v3-pool.json")
+        )
+      case _:
+          self.router_contract = None
+          self.pair_addr = None
+
+
+# #############################
+# ##### GLOBALS
+# #############################
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--config", type=str)
 args = parser.parse_args()
-
-
-#############################
-##### GLOBALS
-#############################
 
 CONFIG = parse_config_file(args.config)
 PRICES = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
@@ -47,19 +117,6 @@ UNISWAP_V3_FEES = {
   # "def": 0.003,
   "xyz": 0.01
 }
-
-
-#############################
-##### UTILS
-#############################
-
-def defaultdict_to_dict(d):
-    if isinstance(d, defaultdict):
-        d = {key: defaultdict_to_dict(value) for key, value in d.items()}
-    return d
-
-def get_now_timestamp():
-  return datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d_%H:%M:%S.%f")
 
 
 #############################
@@ -158,84 +215,71 @@ def arbitrage_engine():
 ##### PRICE MONITOR
 #############################
 
-def connect_to_rpc_node(rpc_url: str):
-    rpc_conn = Web3(Web3.HTTPProvider(rpc_url))
-    if not rpc_conn.is_connected():
-        print("Failed to connect to Ethereum node.")
-        return None
 
-    return rpc_conn
-
-
-def get_abi_from_file(abi_filepath: str):
-    print(f"Getting ABI at {abi_filepath}...")
-    abi = {}
-    try:
-      with open(abi_filepath, "r") as file:
-        abi = json.load(file)
-    except Exception as e:
-      print(f"Error loading pair ABI at {abi_filepath}: {e}")
-      return None
-
-    return abi
-
-
-def monitor_price(
-                  rpc_conn, pair_abi: Any,
-                  exchange_name, pair_name, pair_address: str,
-                  token0_decimals, token1_decimals: int
-  ):
+def monitor_price(pair: UniswapTokenPair):
     global PRICES
-    print(f"Monitoring pair {pair_name} at {pair_address}...")
+    print(f"Monitoring pair {pair.name} of type {pair.type} at {pair.pair_contract.address}...")
 
-    pair_contract = rpc_conn.eth.contract(address=pair_address, abi=pair_abi)
     while True:
-      match exchange_name:
+      match pair.type:
         case "uniswap-v2":
-          price = get_price_uniswap_v2(pair_contract, token0_decimals, token1_decimals)
-
+          price = get_price_uniswap_v2(pair)
         case "uniswap-v3":
-          price = get_price_uniswap_v3(pair_contract, token0_decimals, token1_decimals)
-
-        # case "sushiswap-v3":
-
-        # case "curve":
-
+          price = get_price_uniswap_v3(pair)
         case _:
-          raise Exception(f"Exchange '{exchange_name}' not supported.")
+          raise Exception(f"Token pair type '{pair.type}' not supported.")
 
       if price == None:
-          print(f"{CONFIG['network_name']} - {exchange_name} - {pair_name}: failed to fetch price.")
+          print(f"{CONFIG['network_name']} - {pair.exchange_name} - {pair.name}: failed to fetch price.")
           continue
 
-      print(f"{CONFIG['network_name']} - {exchange_name} - {pair_name}: {price:.18f}")
-      PRICES[CONFIG['network_name']][exchange_name][pair_name] = price
-      PRICES['last_updated'] = get_now_timestamp()
+      print(f"{CONFIG['network_name']} - {pair.exchange_name} - {pair.name}: {price:.18f}")
+      PRICES[CONFIG['network_name']][pair.exchange_name][pair.name] = price
+      # PRICES['last_updated'] = get_now_timestamp()
       time.sleep(CONFIG['monitor_period_ms'] / 1000)
 
 #############################
 ##### DEX CALLS 
 #############################
 
-def get_price_uniswap_v3(pair_contract, token0_decimals, token1_decimals):
+def get_price_uniswap_v3(pair: UniswapTokenPair):
     try:
-        slot0 = pair_contract.functions.slot0().call()
+        slot0 = pair.pair_contract.functions.slot0().call()
         sqrt_price_x96 = slot0[0]
         sqrt_price = sqrt_price_x96 / (2 ** 96)
-        price = (sqrt_price ** 2) * 10**(token0_decimals - token1_decimals)
-        # price = (sqrt_price ** 2) 
+        price = (sqrt_price ** 2) * 10**(pair.token0.decimals - pair.token1.decimals)
         return price
     except Exception as e:
         print(f"Error fetching uniswap-v3 price: {e}")
         return None
 
 
-def get_price_uniswap_v2(pair_contract, token0_decimals, token1_decimals):
+def calculate_amount_out(amount_in, reserve_in, reserve_out):
+    amount_in_with_fee = amount_in * 997
+    numerator = amount_in_with_fee * reserve_out
+    denominator = reserve_in * 1000 + amount_in_with_fee
+    return numerator // denominator
+
+
+def get_price_uniswap_v2(pair: UniswapTokenPair):
     try:
-        reserves = pair_contract.functions.getReserves().call()
-        price = reserves[1] / reserves[0] 
-        price = price * 10**(token0_decimals - token1_decimals)
+        reserves = pair.pair_contract.functions.getReserves().call()
+        reserve_in = reserves[0]
+        decimals_in = pair.token0.decimals
+        reserve_out = reserves[1]
+        decimals_out = pair.token1.decimals
+
+        # base calculation
+        price = reserve_out / reserve_in
+
+        # getAmountOut caculation
+        # amount_in_wei = Web3.to_wei(1, 'ether')
+        # price = calculate_amount_out(amount_in_wei, reserve_in, reserve_out)
+        # price = Web3.from_wei(price, 'ether')
+
+        price = price * 10**(decimals_in - decimals_out)
         return price
+
     except Exception as e:
         print(f"Error fetching uniswap-v2 price: {e}")
         return None
@@ -281,6 +325,29 @@ if __name__ == "__main__":
     print(f"Connecting to {CONFIG['network_name']} RPC Node...")
     rpc_conn = connect_to_rpc_node(CONFIG['rpc_url'])
 
+    # parse config pairs
+    pairs = []
+    for exchange_name, exchange_config in CONFIG['exchanges'].items():
+      token_type = exchange_config['token_type']
+      try:
+        router_address = exchange_config['router_address']
+      except:
+        router_address = None
+
+      for pair_config in exchange_config['pairs']:
+        contract_address = pair_config['address']
+        token0 = ERC20Token(**pair_config['token0'])
+        token1 = ERC20Token(**pair_config['token1'])
+        pairs.append(UniswapTokenPair(
+          token_type,
+          exchange_name,
+          token0,
+          token1,
+          contract_address,
+          router_address=router_address
+        ))
+
+
     #############################
     ##### MAIN THREADS
     #############################
@@ -290,20 +357,10 @@ if __name__ == "__main__":
     server = threading.Thread(target=run_api_srv)
     threads.append(server)
 
-    # monitor threads
-    for exchange_name, exchange_config in CONFIG['exchanges'].items():
-      pair_abi = get_abi_from_file(f"abi/{exchange_config['pair_abi_file']}")
-      for pair_name, pair_config in exchange_config['pairs'].items():
-        monitor = threading.Thread(target=monitor_price, args=(
-            rpc_conn,
-            pair_abi,
-            exchange_name,
-            pair_name,
-            pair_config['address'],
-            pair_config['token0_decimals'],
-            pair_config['token1_decimals'],
-        ))
-        threads.append(monitor)
+    # monitor prices
+    for pair in pairs:
+      monitor = threading.Thread(target=monitor_price, args=(pair,))
+      threads.append(monitor)
 
     # token pair matching
     matching_thread = threading.Thread(target=pair_matching)
